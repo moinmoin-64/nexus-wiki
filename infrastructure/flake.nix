a2076bb435b449b2e96f0496692d09adbc8d2722
Oliver <olist@Oliver.local>
2026-04-13 23:30:17 +0200

    Release v0.1.0: Initial NixOS ISO with auto-update system

--- infrastructure/flake.nix
+++ infrastructure/flake.nix
@@ -1,39 +1,31 @@
 {
-  description = "Project Nexus - Dedicated TUI Server Appliance";
+  description = "Project Nexus - Bootable NixOS Appliance";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
-    nixos-generators = {
-      url = "github:nix-community/nixos-generators";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, nixos-generators }:
-    flake-utils.lib.eachDefaultSystem (system: {
-      packages = {
-        # NixOS Installer ISO
-        iso = (nixos-generators.nixosGenerate {
-          inherit system;
-          modules = [ self.nixosModules.nexus ];
-          format = "iso";
-        }).config.system.build.isoImage;
-
-        # QCOW2 for Proxmox/KVM
-        qcow2 = (nixos-generators.nixosGenerate {
-          inherit system;
-          modules = [ self.nixosModules.nexus ];
-          format = "qcow";
-        }).config.system.build.qcow2Image;
-      };
-
-      defaultPackage = self.packages.${system}.iso;
-    }) // {
-      nixosModules.nexus = ./configuration.nix;
-      nixosConfigurations.nexus = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [ ./configuration.nix ./hardware-configuration.nix ];
-      };
+  outputs = { self, nixpkgs, flake-utils }: 
+    {
+      packages.x86_64-linux = 
+        let
+          nixos = nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+            modules = [
+              "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
+              {
+                isoImage.volumeID = "NEXUS-0.1.0";
+                isoImage.makeEfiBootable = true;
+                isoImage.makeUsbBootable = true;
+                networking.hostName = "nexus";
+              }
+            ];
+          };
+        in
+        {
+          iso = nixos.config.system.build.isoImage;
+          default = nixos.config.system.build.isoImage;
+        };
     };
 }
